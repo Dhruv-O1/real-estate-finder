ee8f53a5b7d03108416cc94aad4e4a0d95f73c92
Dhruv Vaishnav <vaishnavdhruv1144@gmail.com>
2025-02-20 15:11:43 +0530

    integrated navbar and sidebar

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { UserNavbar } from './components/layouts/UserNavbar'
+// import './App.css'
+import './assets/css/adminlte.css'
+import './assets/css/adminlte.min.css'
+import { UserSidebar } from './components/layouts/UserSidebar'
+import { Route, Routes } from 'react-router-dom'
+import { Signup } from './components/common/Signup'
+import { Login } from './components/common/Login'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+      <body className='layout-fixed sidebar-expand-lg bg-body-tertiary sidebar-open app-loadedgi'>
+        <div className='app-wrapper'>
+          <Routes>
+            <Route path='/user' element={<UserSidebar></UserSidebar>}></Route>
+            <Route path='/signup' element={<Signup></Signup>}></Route>
+            <Route path='/login' element={<Login></Login>}></Route>
+          </Routes>
+        </div>
+      </body>
+    
+  )
+}
+
+export default App
