112ce43f053aaff4faa26449abfa785d0bd35dbd
Dhruv Vaishnav <vaishnavdhruv1144@gmail.com>
2025-03-02 20:12:11 +0530

    fixed login and signup aligment by conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { UserNavbar } from './components/layouts/UserNavbar'
-// import './App.css'
+
 import './assets/css/adminlte.css'
 import './assets/css/adminlte.min.css'
 import { UserSidebar } from './components/layouts/UserSidebar'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import { Signup } from './components/common/Signup'
 import { Login } from './components/common/Login'
 import { AddProperty } from './components/user/AddProperty'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // useLocation hook from react router dom to see current end point
+  const location = useLocation()
+
+  // checking if current route is login or signup
+  const isAuthPage = location.pathname === '/login' || location.pathname === '/signup'
 
   return (
     
-      <body className='layout-fixed sidebar-expand-lg bg-body-tertiary sidebar-open app-loaded'>
+      <>
+      {/* Conditionally render the wrappers for proper aligment of login and signup*/}
+        {
+          !isAuthPage ? (
+            <body className='layout-fixed sidebar-expand-lg bg-body-tertiary sidebar-open app-loaded'>
         <div className='app-wrapper'>
           <Routes>
             <Route path='/user' element={<UserSidebar></UserSidebar>}>
               <Route path='add-property' element={<AddProperty/>}></Route>
             </Route>
             
-            <Route path='/signup' element={<Signup></Signup>}></Route>
-            <Route path='/login' element={<Login></Login>}></Route>
           </Routes>
         </div>
       </body>
+          ) : (
+            <Routes>
+              {/* routes with centered layout */}
+            <Route path='/signup' element={<Signup></Signup>}></Route>
+            <Route path='/login' element={<Login></Login>}></Route>
+            </Routes>
+          )
+        }
+      </>
     
   )
 }
--- src/components/common/Signup.jsx
+++ src/components/common/Signup.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
 export const Signup = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
